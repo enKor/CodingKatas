29009a3d170ca3e92ce85bfeaa25e773c5791ad9
Martin Procházka <prochazka@cngroup.dk>
2022-01-09 08:44:00 +0100

    Diamond

---
+++ Diamond/Diamond/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Diamond
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Draw('C');
+        }
+
+        private static void Draw(char c)
+        {
+
+
+
+        }
+
+
+    }
+}
